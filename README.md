c39db0f0cb12367fbe7f755592285c252f180117
Dawid Bobowski <dbobowski3@gmail.com>
2023-06-18 22:14:42 +0200

    Add empty images and results folders to the repo; update README; update CLI print messages

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # image-ai-upscaler
 
 Image AI Upscaler is a simple Python CLI program which upscales images with JPG and PNG extensions using your GPU (or CPU if no CUDA GPU is available). Currently it has a hardcoded x4 upscale algorithm. \
-picturs\
-Put your images inside of images folder and run main.py file. Your upscaled images are going to be located inside results folder.
+\
+Before you run the program:
+
+- create **_images_** and **_results_** folders inside of the main directory;
+- put your images inside of **_images_** folder;
+- run main.py file.
+
+Your upscaled images are going to be located inside of the **_results_** folder.
 
 ## To do:
 
--- main.py
+++ main.py
@@ -7,23 +7,23 @@ supported_extensions = ["png", "jpg"]
 images_path = "images/"
 results_path = "results/"
 
-print("Loading AI model...")
+print("Loading AI model... ", end="")
 device = torch.device("cpu")
 model = RealESRGAN(device, scale=4)
 model.load_weights("weights/RealESRGAN_x4.pth", download=True)
-print("Model loaded.")
+print("Model loaded!")
 
 directory = os.fsencode(images_path)
 for file in os.listdir(directory):
     file_name = os.fsdecode(file)
     extension = file_name.split(".")[-1]
     if extension in supported_extensions: 
-        print(f"{file_name}: file OK.")
+        print(f"{file_name}: file OK. ", end="")
         image = Image.open(f"{images_path}{file_name}").convert("RGB")
-        print("Starting upscaling process...")
+        print("Starting upscaling process... ", end="")
         sr_image = model.predict(image)
         sr_image.save(f"{results_path}{file_name}")
         print("Image saved!")
     else:
         print (f"{file_name}: file extension not supported.")
-print("All files were processed. Exitting. Thank you!")
+print("All files were processed. Exitting. Thank you for using AI Image Upscaler!")
