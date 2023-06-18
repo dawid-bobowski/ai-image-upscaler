892b0862b210f2f0597d66838bc5cd8499549ed5
Dawid Bobowski <dbobowski3@gmail.com>
2023-06-19 00:42:10 +0200

    Support cuda device, upscale multiplier choice; move results to images/upscaled folder

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-# image-ai-upscaler
+# ai-image-upscaler
 
-Image AI Upscaler is a simple Python CLI program which upscales images with JPG and PNG extensions using your GPU (or CPU if no CUDA GPU is available). Currently it has a hardcoded x4 upscale algorithm. \
-\
-Before you run the program:
+AI Image Upscaler is a simple Python CLI program which upscales images with JPG and PNG extensions using your GPU (or CPU if no CUDA GPU is available). Currently it allows to use x2, x4 or x8 upscale algorithms.
 
-- create **_images_** and **_results_** folders inside of the main directory;
+## To use a GPU mode:
+
+- before doing anything, firstly install CUDA Toolkit 11.7 from Nvidia (the only tested version) available here: https://developer.nvidia.com/cuda-11-7-0-download-archive
+
+## Before you run the program:
+
+- go to ai-image-upscaler main directory;
+- run `py -m pip install -r requirements.txt` (it will take a few minutes to install depedning on your Internet connection speed);
 - put your images inside of **_images_** folder;
-- run main.py file.
+- run: `py main.py`;
+- choose upscale multiplier from x2, x4 or x8;
+- wait for the results.
 
-Your upscaled images are going to be located inside of the **_results_** folder.
-
-## To do:
-
-- make it possible to choose between x2, x4 and x8 upscale multiplier.
+Your upscaled images are going to be located inside of **_images/upscaled_** folder with their names appended with "-upscaled".
--- main.py
+++ main.py
@@ -3,27 +3,55 @@ from PIL import Image
 from RealESRGAN import RealESRGAN
 import os
 
-supported_extensions = ["png", "jpg"]
-images_path = "images/"
-results_path = "results/"
+GPU_DEVICE = "cuda"
+CPU_DEVICE = "cpu"
 
+supported_extensions = ["png", "jpg"]
+supported_upscale_multipliers = ["2", "4", "8"]
+images_path = "images/"
+
+# load a device
+print("Loading device... ", end="")
+device_type = GPU_DEVICE if torch.cuda.is_available() else CPU_DEVICE
+print(f"Selected model type is: {device_type}.")
+print(f"Starting device... ", end="")
+device = torch.device(device_type)
+if device_type == GPU_DEVICE:
+    tensor = torch.tensor([1, 2, 3])
+    tensor = tensor.to(device)
+print("Device is ready!\n")
+
+# set up an upscale multiplier
+while True:
+  multiplier = input("How much would you like to upscale your images? Available options are: 2, 4 or 8 (press Enter for default=4): ")
+  if multiplier not in [supported_upscale_multipliers, ""]:
+      print("Your choice has to be one of 2, 4 (default) or 8.")
+      continue
+  break
+if multiplier == "":
+    multiplier = "4"
+print(f"Chosen upscale multiplier is x{multiplier}.\n")
+
+# load an ai model
 print("Loading AI model... ", end="")
-device = torch.device("cpu")
-model = RealESRGAN(device, scale=4)
-model.load_weights("weights/RealESRGAN_x4.pth", download=True)
+model = RealESRGAN(device, scale=int(multiplier))
+model.load_weights(f"weights/RealESRGAN_x{multiplier}.pth", download=True)
 print("Model loaded!")
 
+# upscale images
+print("Starting upscaling process...\n")
 directory = os.fsencode(images_path)
 for file in os.listdir(directory):
     file_name = os.fsdecode(file)
     extension = file_name.split(".")[-1]
     if extension in supported_extensions: 
-        print(f"{file_name}: file OK. ", end="")
+        print(f"{file_name}: file OK.\nProcessing... ", end="")
         image = Image.open(f"{images_path}{file_name}").convert("RGB")
-        print("Starting upscaling process... ", end="")
         sr_image = model.predict(image)
-        sr_image.save(f"{results_path}{file_name}")
+        sr_image.save(f"{images_path}upscaled/{file_name.split('.')[-2]}-upscaled.{extension}")
         print("Image saved!")
+    if file_name == ".gitignore":
+        continue
     else:
         print (f"{file_name}: file extension not supported.")
-print("All files were processed. Exitting. Thank you for using AI Image Upscaler!")
+print("\nAll files were processed. Exitting. Thank you for using AI Image Upscaler!")
